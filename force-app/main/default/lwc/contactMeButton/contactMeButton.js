2dfe40841a3172f4d76b6596ba0fa6313e9834f8
suneel9491 <suneelkumar4502@gmail.com>
2023-08-27 22:48:41 +0530

    snackbar(message sent successfully)

--- force-app/main/default/lwc/contactMeButton/contactMeButton.js
+++ force-app/main/default/lwc/contactMeButton/contactMeButton.js
@@ -8,6 +8,7 @@ export default class ContactMeButton extends LightningElement {
     company;
     email;
     description;
+    snackbar;
 
     renderedCallback(){
 this.dialog = this.template.querySelector('.contact-dialog');
@@ -16,6 +17,7 @@ this.lastname = this.template.querySelector('.lastname');
 this.company = this.template.querySelector('.company');
 this.email = this.template.querySelector('.email');
 this.description = this.template.querySelector('.description');
+this.snackbar = this.template.querySelector('c-snackbar');
     }
 
     showDialog(){
@@ -42,6 +44,8 @@ createLead({
         description:descriptionValue
 
 
+    }).then(() => {
+        this.snackbar.showSnackBar('Your request has been received.')
     });
     this.closeDialog();
     }
