ac66607226e25f08a24686d01bc514bfb3eb7a7b
suneel9491 <suneelkumar4502@gmail.com>
2023-08-28 13:36:08 +0530

    skillcard updated

--- force-app/main/default/lwc/hero/hero.js
+++ force-app/main/default/lwc/hero/hero.js
@@ -1,6 +1,7 @@
 import { LightningElement } from 'lwc';
 import IMAGES from '@salesforce/resourceUrl/Images';
 export default class Hero extends LightningElement {
-imageURL = IMAGES + '/hero.png';
+
+    imageURL = IMAGES + '/hero.png';
 
 }
---
+++ force-app/main/default/lwc/skillCards/skillCards.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/Images';
+
+export default class SkillCards extends LightningElement {
+
+    adminImageURL = IMAGES + '/admin.png';
+    trainingImageURL = IMAGES + '/training.png';
+    serviceImageURL = IMAGES + '/service.png';
+}
